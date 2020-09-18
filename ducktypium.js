fd93531ef7f7034055cc7e2adbd6046c87641d41
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-09-17 20:37:12 -0700

    Completed the ducktypium challenge

--- ducktypium.js
+++ ducktypium.js
@@ -1,18 +1,15 @@
 class Ducktypium {
   constructor(crystalColor) {
-    this.color = crystalColor;
-    this.calibrationSequence = [];
-    try {
-      if (
-        this.color !== 'red' &&
-        this.color !== 'blue' &&
-        this.color !== 'yellow'
-      ) {
-        throw 'Wrong color';
-      }
-    } catch (error) {
-      return error;
+    if (crystalColor === 'red') {
+      this.color = crystalColor;
+    } else if (crystalColor === 'blue') {
+      this.color = crystalColor;
+    } else if (crystalColor === 'yellow') {
+      this.color = crystalColor;
+    } else {
+      throw 'Wrong color';
     }
+    this.calibrationSequence = [];
   }
 
   refract(newColor) {
@@ -42,7 +39,21 @@ class Ducktypium {
       return 'green';
     }
   }
+
+  calibrate(arr) {
+    arr.sort((a, b) => a - b);
+    this.calibrationSequence = arr.map((e) => e * 3);
+  }
 }
 
-const m = new Ducktypium('p');
-console.log(m.refract('p'));
+// Create a new instance of the class
+const dt = new Ducktypium('blue');
+
+console.log(dt.color); // prints 'red'
+
+console.log(dt.refract('blue')); // prints 'purple'
+console.log(dt.refract('red')); // prints 'red'
+
+dt.calibrate([3, 5, 1]);
+
+console.log(dt.calibrationSequence); // prints [3, 9, 15]
